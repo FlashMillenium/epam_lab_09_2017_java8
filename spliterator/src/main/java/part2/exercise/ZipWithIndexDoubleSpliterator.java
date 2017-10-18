552ea5267a24788eba7122058935ebfc7ed88bdd
mal <flash.millenium@gmail.com>
2017-10-18 21:58:14 +0300

    add foreachremaining for unsized stream

--- spliterator/src/main/java/part2/exercise/ZipWithIndexDoubleSpliterator.java
+++ spliterator/src/main/java/part2/exercise/ZipWithIndexDoubleSpliterator.java
@@ -1,8 +1,10 @@
 package part2.exercise;
 
+import java.util.Objects;
 import java.util.Spliterator;
 import java.util.Spliterators;
 import java.util.function.Consumer;
+import java.util.function.DoubleConsumer;
 
 public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSpliterator<IndexedDoublePair> {
 
@@ -22,20 +24,38 @@ public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSplitera
 
     @Override
     public int characteristics() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return inner.characteristics();
     }
 
     @Override
     public boolean tryAdvance(Consumer<? super IndexedDoublePair> action) {
-        // TODO
-        throw new UnsupportedOperationException();
+        boolean result = inner.tryAdvance((DoubleConsumer) e->
+                action.accept(new IndexedDoublePair(currentIndex,e)));
+        currentIndex++;
+        return result;
     }
 
     @Override
     public void forEachRemaining(Consumer<? super IndexedDoublePair> action) {
         // TODO
-        throw new UnsupportedOperationException();
+        int i = currentIndex;
+        if(inner.hasCharacteristics(Spliterator.SIZED)){
+            for (; i<inner.estimateSize();i++){
+                final int x = i;
+                inner.tryAdvance((DoubleConsumer) e->
+                        action.accept(new IndexedDoublePair(x,e)));
+            }
+            currentIndex=i;
+        } else {
+            boolean next;
+           do{
+               final int x = i;
+               next=inner.tryAdvance((DoubleConsumer) e->
+                       action.accept(new IndexedDoublePair(x,e)));
+               i++;
+           }
+           while (next);
+        }
     }
 
     @Override
@@ -45,12 +65,20 @@ public class ZipWithIndexDoubleSpliterator extends Spliterators.AbstractSplitera
         //   use inner.trySplit
         // } else
 
-        return super.trySplit();
+        if (inner.hasCharacteristics(Spliterator.SIZED) && inner.hasCharacteristics(Spliterator.SUBSIZED)){
+            OfDouble leftInner = inner.trySplit();
+            if(Objects.isNull(leftInner)) return null;
+            ZipWithIndexDoubleSpliterator result = new ZipWithIndexDoubleSpliterator(currentIndex,leftInner);
+            currentIndex=(int)(currentIndex+leftInner.estimateSize());
+            return result;
+        }else
+        {
+            return null;
+        }
     }
 
     @Override
     public long estimateSize() {
-        // TODO
-        throw new UnsupportedOperationException();
+        return inner.estimateSize();
     }
 }
